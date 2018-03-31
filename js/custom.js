62c0dff9204f6f02fff77bfee34e4521c171323a
Lailah <lailahfsf@gmail.com>
2018-03-31 11:25:28 +0100

    fixed structure to be more pleasant

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+/**
+ * Custom Javascript for Booklist
+ */
+
+// Self-executing Function to run last
+(function () {
+    // This __SHOULD__ only run when DOM is done loading
+    console.log("Hello World!")
+})()
